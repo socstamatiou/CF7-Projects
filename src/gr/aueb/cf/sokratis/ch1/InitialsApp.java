ccd834a200e8515c8b5a6e628962c9619730393c
sokratis <socstamatiou@gmail.com>
2025-01-31 22:42:49 +0200

    Ασκήσεις ch1

---
+++ src/gr/aueb/cf/sokratis/ch1/InitialsApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.sokratis.ch1;
+
+/**
+ * Εκτυπώνει τα αρχικά του ονοματεπώνυμου Σταματίου Σωκράτης
+ */
+
+public class InitialsApp {
+    public static void main(String[] args) {
+        System.out.println("*******  *******");
+        System.out.println(" *        *");
+        System.out.println("  *        *");
+        System.out.println(" *        *");
+        System.out.println("*******  *******");
+    }
+}
---
+++ src/gr/aueb/cf/sokratis/ch1/MenuApp.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.sokratis.ch1;
+
+/**
+ * Εμφανίζει ένα μενού επιλογών
+ */
+
+public class MenuApp {
+    public static void main(String[] args) {
+        System.out.println("Επιλέξτε μία από τις παρακάτω επιλογές:");
+        System.out.println("1. Εισαγώγή");
+        System.out.println("2. Διαγραφή");
+        System.out.println("3. Αναζήτηση");
+        System.out.println("4. Ενημέρωση");
+        System.out.println("5. Έξοδος");
+        System.out.println("Δώστε αριθμό επιλόγής:");
+    }
+}
