28af4c833dd624e8d50b86733a5f5b123665126e
sokratis <socstamatiou@gmail.com>
2025-01-31 22:43:03 +0200

    Ασκήσεις ch1

---
+++ src/gr/aueb/cf/sokratis/ch1/sumApp.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.sokratis.ch1;
+
+/**
+ * Προσθέτει δύο ακεραίου και εμφανίζει το
+ * αποτέλεσμα στην οθόνη (κονσόλα)
+ */
+
+public class sumApp {
+    public static void main(String[] args) {
+        //Δήλωση και αρχικοποίηση μεταβλητών
+        int num1 = 19;
+        int num2 = 30;
+        int sum = num1 + num2;
+
+        //Εκτύπωση αποτελέσματος
+        System.out.println("Το αποτέλεσμα της πρόσθεσης είναι ίσο με:" + sum);
+    }
+}
