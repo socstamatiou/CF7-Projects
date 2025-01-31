dc63c4018d679cd93f4f003df9f9ec686201705b
sokratis <socstamatiou@gmail.com>
2025-01-31 22:14:49 +0200

    Initial Commit

---
+++ src/gr/aueb/cf/sokratis/ch1/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.sokratis.ch1;
+
+/**
+ * Prints the phrase 'Hello World' in the standard outpup (console)
+ *
+ * @author socstamatiou
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
